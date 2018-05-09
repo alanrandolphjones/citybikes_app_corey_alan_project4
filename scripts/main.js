e14eadd6a7b29dfbb19e570197115466da3beb8b
Alan Jones <jones.alan21@gmail.com>
2018-05-09 14:41:21 -0400

    used promises

--- scripts/main.js
+++ scripts/main.js
@@ -3,53 +3,59 @@
 
 const app = {};
 
-app.loadMarkers = () => {
+app.events = () => {
 
-    //Call Watch position and turn position into a marker
+    const locationPromise = new Promise((resolve, reject) => {
+        navigator.geolocation.watchPosition(function (pos) {
 
-
-    navigator.geolocation.watchPosition(function (position) {
-
-        const latit = position.coords.latitude
-
-        const longit = position.coords.longitude
-
-        const home = new google.maps.Marker({
-            position: new google.maps.LatLng(latit, longit),
-            map: app.map, // notice how we pass it the map we made earlier? This is how it knows which map to put the marker on
-            icon: 'your_location_marker.png'
+            resolve(pos);
 
         })
+    })
+
+    locationPromise.then((pos) => {
+
+        const lat1 = pos.coords.latitude
+
+        const lng1 = pos.coords.longitude
+
+        app.getMap(lat1, lng1)
+
+        app.getMarkers(lat1, lng1)
 
     })
 
 }
 
-app.loadMap = function () {
-    // Call current location, then input the position into a map object
 
-    navigator.geolocation.getCurrentPosition(function (position) {
+app.getMarkers = (lat1, lng1) => {
 
-        const latit = position.coords.latitude
-
-        const longit = position.coords.longitude
-
-        const mapOptions = {
-            center: { lat: latit, lng: longit },
-            zoom: 17
-        }
-
-        const $mapDiv = $('#map')[0]
-
-        app.map = new google.maps.Map($mapDiv, mapOptions);
-
-        app.loadMarkers()
-    
+    app.home = new google.maps.Marker({
+        position: new google.maps.LatLng(lat1, lng1),
+        map: app.map, // notice how we pass it the map we made earlier? This is how it knows which map to put the marker on
+        icon: 'your_location_marker.png'
     })
+}
 
+app.getMap = function (lat1, lng1) {
+    // Call current location, then input the position into a map object
+    const mapOptions = {
+        center: { lat: lat1, lng: lng1 },
+        zoom: 17
+    }
+
+    const $mapDiv = $('#map')[0]
+
+    app.map = new google.maps.Map($mapDiv, mapOptions);
+}
+
+
+app.init = () => {
+
+    app.events()
 }
 
 $(function () {
     // 4. call load map when the document is ready
-    app.loadMap();
+    app.init()
 });
